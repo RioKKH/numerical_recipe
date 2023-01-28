a407ef3dfd5aa7b1be7926bbd2cc0231fddeb1f6
Ryoichi Kakehi <ryoichikakehi@gmail.com>
2023-01-28 18:05:24 +0900

    modified:   program1.7_machine_epsilon.cpp

--- program1.7_machine_epsilon.cpp
+++ program1.7_machine_epsilon.cpp
@@ -6,3 +6,19 @@ int main(void)
     double dtmp;       // 作業変数 double型)
     float feps = 1.0f; // マシンイプシロン (float型)
     float ftmp;        // 作業用変数 (float型)
+
+    // double型のマシンイプシロンの計算
+    // deps + 1 > 1の間、depsを2で割り続ける
+    // {}で処理を書かなくてもfor()だけで所望の計算をすることが出来る
+    for (dtmp = deps + 1.0; dtmp > 1; deps /= 2.0, dtmp = deps + 1.0);
+    printf("double型のMachine epsilonは%-16g\n", 2.0*deps);
+    printf("double型のUnit roundoffは%-16g\n", deps);
+
+    // float型のマシンイプシロンの計算
+    // feps + 1 > 1の間、fepsを2で割り続ける
+    for (ftmp = feps + 1.0; ftmp > 1; feps /= 2.0, ftmp = feps + 1.0);
+    printf("float型のMachine epsilonは%-16g\n", 2.0*feps);
+    printf("float型のUnit roundoffは%-16g\n", feps);
+
+    return 0;
+}
