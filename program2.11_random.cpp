2d7d75577a00c4b4e71ee89e3d8eba6f03b8e761
Ryoichi Kakehi <ryoichikakehi@gmail.com>
2023-03-12 15:41:07 +0900

    modified:   program2.11_random.cpp

--- program2.11_random.cpp
+++ program2.11_random.cpp
@@ -5,9 +5,21 @@
 int main(void)
 {
     int i;
+
     printf("シード無し\n");
     for (i = 1; i <= 2; i++)
     {
-        printf("%d\n", rand());
+        printf("%d\t", rand());
     }
-    printf("シード無し\n");
+    printf("\n");
+
+    printf("シード有り\n");
+    srand((unsigned)time(NULL));
+    for (i = 1; i <= 2; i++)
+    {
+        printf("%d\t", rand());
+    }
+    printf("\n");
+
+    return 0;
+}
